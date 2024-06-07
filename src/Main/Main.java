c07093fa8935007f16a076824beacec889c1935d
andres2204 <medinaandres2204@gmail.com>
2024-06-06 21:45:31 -0500

    documentacion de conexion

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,83 +1,13 @@
 package Main;
 
 import mariadbConnection.MariadbConnection;
+import terminalUtils.TerminalUtils;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        new MariadbConnection("IP DEL SERVER", 3306, "PruebasU", "SU USER CAREMONDAS", "PONGAN SU PASSWORD").connect();
-        /*
-        Connection conn = null;
-        Statement sentencia = null;
-        ResultSet resultado = null;
-
-        String host = "127.0.0.1";
-        String port = "1521";
-        String serviceName = "xe";
-        String url = "jdbc:oracle:thin:@//" + host + ":" + port + "/" + serviceName;
-        String user = "C##LAU";
-        String password = "hola123";
-
-        System.out.println("[!] Conexión a la base de datos...");
-
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("[+] Conexión Exitosa!");
-
-
-            sentencia = conn.createStatement();
-            System.out.println("Seleccionando...");
-
-            // Consulta simple para verificar la conexión y acceso a la tabla
-            resultado = sentencia.executeQuery("select * from cliente");
-            if (resultado.next()) {
-                System.out.println("conn -> " + resultado.getObject(1));
-                int count = resultado.getInt(1);
-                System.out.println("Número de registros en la tabla Lavado: " + count);
-            }
-
-            // Realiza la consulta principal
-            resultado = sentencia.executeQuery("SELECT * FROM Lavado");
-            while (resultado.next()) {
-                System.out.println("ID: " + resultado.getObject(1) + ", Tipo_Lavado: " + resultado.getString("Tipo_Lavado"));
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("ERROR -> Clase del controlador no encontrada: " + e.getMessage());
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("ERROR -> SQL Exception: " + e.getMessage());
-        } finally {
-            // Cerrar ResultSet
-            if (resultado != null) {
-                try {
-                    resultado.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            // Cerrar Statement
-            if (sentencia != null) {
-                try {
-                    sentencia.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            // Cerrar Connection
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        */
-
-        System.out.println("Consulta finalizada.");
+        new MariadbConnection("192.168.128.12", 3306, "PruebasU", "admin", "andresUser").connect();
+        TerminalUtils.infoTrace("Bye");
     }
 }
--- src/mariadbConnection/MariadbConnection.java
+++ src/mariadbConnection/MariadbConnection.java
@@ -6,6 +6,36 @@ import java.sql.*;
 
 public class MariadbConnection {
 
+    /**
+     * DOCUMENTACION MARIADB
+     *      Para conectarse a la base de datos, tienes que tener presente lo siguiente:
+     *          - Tener un usuario creado con privileguios sobre la base de datos:
+     *
+     *                  crear usuario:
+     *                      create user admin@'IP DE CONEXION EN CASO DE QUE NO SEA LOCALHOST' identified by 'andresUser'
+     *                          - Ejemplo de ip: 192.168.128.% donde el % representa /24 (dejen el %)
+     *
+     *
+     *                  privileguios:
+     *                      GRANT ALL PRIVILEGES ON NOMBRE_DB.* TO 'USERNAME'@'IP COMO LA DEL USUARIO O %' IDENTIFIED BY 'PASSWORD';
+     *
+     *          - Tener el servicio de mariadb corriendo (esa se las dejo si estan en windows)
+     *
+     *          - Si se van a conectar desde otra maquina, configurar el bind-address = 0.0.0.0 o a la ip espesifica del cliente
+     *                  - se puede configurar en el archivo /etc/mysql/mariadb.conf.d/50-server.cnf (en linux)
+     *
+     *
+     * TODO: ESTA VAINALOCA
+     * Triggers:
+     *   - cuando se inserte, enseguida calcular el valor dependiendo del tipo de vehiculo (moto, carro, camion), tipo de lavado (interior, exterior, completo) y tipo cliente (premiun 20%, estandar 0%)
+     *   -
+     *
+     * Procedimientos:
+     *   - insertar
+     *   - consultar datos
+     *
+    */
+
 
     // atributes
     private String host;
@@ -41,6 +71,8 @@ public class MariadbConnection {
             query = connection.createStatement();
             res = query.executeQuery("select * from producto");
 
+            exeQuery("select");
+
             while (res.next()) {
                 System.out.println("ID: " + res.getObject(1) + ", nombre: " + res.getString(2));
             }
@@ -50,7 +82,17 @@ public class MariadbConnection {
         }
     }
 
-    private void insert(Statement query) throws SQLException {
+    /**
+     *
+     * @param sql {@code SELECT} statement
+     * @throws SQLException
+     */
+    private ResultSet exeQuery(String sql) throws SQLException {
+        return query.executeQuery(sql);
+
+    }
+
+    private void exe(String sql) throws SQLException {
         query.execute("INSERT INTO producto VALUES(12, 'Insersion Java', 323, 3)");
     }
 }
