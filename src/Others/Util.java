b0ba8b8c1aa576fb5bdc90ebd2a5ac01a47242b9
darwingtx <dc162005@gmail.com>
2024-06-10 22:52:13 -0500

    pequeños cambios en el oracle y mariadb

---
+++ src/Others/Util.java
@@ -0,0 +1,47 @@
+package Others;
+
+import terminalUtils.ColumnFormat;
+import terminalUtils.TerminalUtils;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Util {
+
+    public static String showResponse(ResultSet response) throws SQLException {
+
+        if (response == null) return "Empty Set";
+
+        ResultSetMetaData resData = response.getMetaData();
+
+        int columnCount  = resData.getColumnCount();
+        ArrayList<ArrayList<String>> data = new ArrayList<>();
+        ArrayList<ColumnFormat> columnsFormat = new ArrayList<>();
+
+        // extract data
+        int[] maxLengthColumn = new int[columnCount];
+        for (int i = 0; i < columnCount; i++) {
+            maxLengthColumn[i] = resData.getColumnName(i+1).length();
+        }
+
+        int row = 0;
+        while (response.next()) {
+            ArrayList<String> rowData = new ArrayList<>();
+            for (int i = 1; i <= columnCount; i++) {
+                String d = response.getObject(i) + "";
+                rowData.add(d);
+                if (d.length() > maxLengthColumn[i-1]) maxLengthColumn[i-1] = d.length();
+            }
+            data.add(rowData);
+            row++;
+        }
+
+        for (int i = 1; i <= columnCount; i++) {
+            columnsFormat.add(new ColumnFormat(maxLengthColumn[i-1], resData.getColumnName(i), ""));
+        }
+
+        return new TerminalUtils(columnsFormat).printTable(data, false);
+    }
+}
