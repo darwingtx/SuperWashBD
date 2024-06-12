bf41030c6f83b72db9ea3cf8ad6b6c2bb288c9ea
PARZEV4L <andravid019@gmail.com>
2024-06-12 18:52:07 -0500

    Menu insertar, falla en lavado

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,16 +8,8 @@ import Menu.Menu;
 
 public class Main {
     public static void main(String[] args) {
-     //Menu menu = new Menu();
-     //menu.menu();
-        Lavado x = new Lavado(2, 1015332154,"ABC123");
-        OracleConnection db = new OracleConnection(
-                System.getenv("DB_HOST"),
-                Integer.parseInt(System.getenv("DB_PORT")),
-                System.getenv("DB_NAME"),
-                System.getenv("DB_USR"),
-                System.getenv("DB_PASS"));
-        db.connect();
-        db.insertLavado(x);
+     Menu menu = new Menu();
+     menu.menu();
+
     }
 }
--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -6,6 +6,7 @@ import DataBaseConnection.*;
 import terminalUtils.TerminalUtils;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Menu {
@@ -37,24 +38,23 @@ public class Menu {
                         System.getenv("DB_USR"),
                         System.getenv("DB_PASS"));
                 db = db.connect();
-                if(db==null){
+                if (db == null) {
                     TerminalUtils.infoTrace("------[+] Conexión Erronea!, verifica credenciales------");
-                }else {
+                } else {
                     menuDB(db, opts);
                 }
                 break;
 
             case "Trabajara con Oracle Database":
                 db = new OracleConnection(
-                        System.getenv("DB_HOST"),
-                        Integer.parseInt(System.getenv("DB_PORT")),
-                        System.getenv("DB_NAME"),
-                        System.getenv("DB_USR"),
-                        System.getenv("DB_PASS"));
+                        "127.0.0.1", 1521,
+                        "SQLFinal",
+                        "PARZ",
+                        "1015332154");
                 db = db.connect();
-                if(db==null){
+                if (db == null) {
                     TerminalUtils.infoTrace("------[+] Conexión Erronea!, verifica credenciales------");
-                }else {
+                } else {
                     menuDB(db, opts);
                 }
                 break;
@@ -72,15 +72,19 @@ public class Menu {
                     break;
 
                 case "Ingresar cliente":
-
+                    Cliente cliente = obtenerCliente();
+                    x.insertClient(cliente);
                     break;
 
                 case "Ingresar Vehiculo":
+                    Vehiculo vehiculo = obtenerVehiculo();
+                    x.insertVeh(vehiculo);
 
                     break;
 
                 case "Ingresar Registro de Lavado ":
-
+                    Lavado lavado = obtenerLavado();
+                    x.insertLavado(lavado);
                     break;
 
                 case "Listar Registros de Lavado ":
@@ -98,10 +102,9 @@ public class Menu {
         }
     }
 
-
     public static Cliente obtenerCliente() {
         Cliente c;
-        String[] tipo_cliente = {"Estandar", "Premiun"};
+        String[] tipo_cliente = { "Estandar", "Premiun" };
         c = new Cliente(input("id cliente"), menuOpciones(tipo_cliente), input("nombre"), input("apellido"),
                 input("Telefono"), input("descuento"));
         return c;
@@ -109,7 +112,7 @@ public class Menu {
 
     public static Vehiculo obtenerVehiculo() {
         Vehiculo v;
-        String[] marca = {"Mercedez ven10", "whatsapp", "taxi"};
+        String[] marca = { "Mercedez ven10", "whatsapp", "taxi" };
         v = new Vehiculo(input("Id vehiculo"), input("id cliente"), menuOpcionesString(marca),
                 input("Tipo_vehiculo"));
         return v;
@@ -117,28 +120,34 @@ public class Menu {
 
     public static Lavado obtenerLavado() {
         Lavado l = null;
-
-        //l = new Lavado(input("id Lavado"), input("id vehiculo"), inputInt(null), inputInt(null), input("Precio"),
-                //input("Duracion"));
+        String[] Tlav = { "Exterior", "Interior", "Completo" };
+        l = new Lavado(menuOpciones(Tlav), inputInt("Id Cliente"), input("Id Vehiculo"));
         return l;
     }
 
-    public static int menuOpciones(String[] opts) {
-        boolean pass = false;
-        int opt;
-        Scanner r = new Scanner(System.in);
-        do {
-            for (int i = 0; i < opts.length; i++) {
-                System.out.println((i + 1) + ". " + opts[i]);
-            }
-            System.out.print("Escriba su opcion: ");
-            opt = r.nextInt();
-            if (opt >= 0 && opt < opts.length)
-                pass = true;
+    public static int menuOpciones(String[] opciones) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion = -1;
 
-        } while (!pass);
-        r.close();
-        return opt;
+        while (true) {
+            for (int i = 0; i < opciones.length; i++) {
+                System.out.println((i + 1) + ". " + opciones[i]);
+            }
+            System.out.print("Escriba su opción: ");
+            try {
+                opcion = scanner.nextInt() - 1;
+                if (opcion >= 0 && opcion < opciones.length) {
+                    break;
+                } else {
+                    System.out.println("Opción inválida. Por favor, intente de nuevo.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, intente de nuevo.");
+                scanner.next(); 
+            }
+        }
+
+        return opcion;
     }
 
     public static String menuOpcionesString(String[] opciones) {
@@ -159,7 +168,7 @@ public class Menu {
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Entrada inválida. Por favor, intente de nuevo.");
-                scanner.next(); // Limpiar la entrada inválida
+                scanner.next(); 
             }
         }
 
@@ -170,7 +179,6 @@ public class Menu {
         Scanner r = new Scanner(System.in);
         System.out.println("Ingrese " + atr + ": ");
         String d = r.nextLine();
-        r.close();
         return d;
     }
 
@@ -178,7 +186,6 @@ public class Menu {
         Scanner r = new Scanner(System.in);
         System.out.println("Ingrese " + atr + ": ");
         int d = r.nextInt();
-        r.close();
         return d;
     }
 }
